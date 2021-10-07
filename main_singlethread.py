312f5fd981fea710da9ba4f3ca009161a237f51e
İsmail Tosun <ismailtosuntnyl@gmail.com>
2021-10-08 02:56:37 +0300

    single thread calculation with join

--- main_singlethread.py
+++ main_singlethread.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
     n = 10000000
 
     clock.tic()
-    Thread(target=find_e.throw_points, args=(n,)).start()
-
+    thread = Thread(target=find_e.throw_points, args=(n,))
+    thread.start()
+    thread.join()
     print("e = %12.7f | counter = %d | N = %d" %
           (find_e.calculate_e(), find_e.counter, find_e.n))
     print("TIME = %.6f seconds" % (clock.toc()))
