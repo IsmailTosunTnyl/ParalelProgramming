4117063fb891dd31a2d6bf6b9dc703dea1bf6812
İsmail Tosun <ismailtosuntnyl@gmail.com>
2021-10-08 02:38:33 +0300

    regular calculation

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from it import *
 
-find_e = FindE()
-clock = TicToc()
+if __name__ == '__main__':
+    find_e = FindE()
+    clock = TicToc()
 
-n = 10000000
+    n = 10000000
 
-clock.tic()
-e = find_e.throw_points(n)
+    clock.tic()
+    e = find_e.throw_points(n)
 
-
-print("e = %12.7f | counter = %d | N = %d" %
-      (e, find_e.counter, n))
-print("TIME = %.6f seconds" % (clock.toc()))
-
-
-
+    print("e = %12.7f | counter = %d | N = %d" %
+          (e, find_e.counter, n))
+    print("TIME = %.6f seconds" % (clock.toc()))
